ddf65506a0d65cae40880f72dca8327b0d0bec46
samuelchou <samuelchou@hotmail.com.tw>
2020-10-08 16:54:09 +0800

    <Add> ActivityList初步提供Adapter綁定，並呈現資料。

--- app/src/main/java/studio/ultoolapp/examplemvvm/view/ActivityList.java
+++ app/src/main/java/studio/ultoolapp/examplemvvm/view/ActivityList.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import studio.ultoolapp.examplemvvm.R;
 import studio.ultoolapp.examplemvvm.databinding.ActivityListBinding;
+import studio.ultoolapp.examplemvvm.viewmodel.ItemAdapter;
 import studio.ultoolapp.examplemvvm.viewmodel.ListViewModel;
 
 public class ActivityList extends AppCompatActivity {
     private ActivityListBinding binding;
     private ListViewModel listViewModel;
+    private ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,10 @@ public class ActivityList extends AppCompatActivity {
         listViewModel = new ListViewModel(this);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_list);
         binding.setListModel(listViewModel);
+
+        itemAdapter = new ItemAdapter(listViewModel.getItems());
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        binding.recyclerView.setAdapter(itemAdapter);
     }
 
     @Override
