233d590b9fd3e8cb2c83dcc62eb15857905907f6
samuelchou <samuelchou@hotmail.com.tw>
2020-10-08 15:33:48 +0800

    <Add> 填充Adapter項，並於item layout提供一個對應View。

--- app/src/main/java/studio/ultoolapp/examplemvvm/viewmodel/ItemAdapter.java
+++ app/src/main/java/studio/ultoolapp/examplemvvm/viewmodel/ItemAdapter.java
@@ -1,36 +1,55 @@
 package studio.ultoolapp.examplemvvm.viewmodel;
 
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import studio.ultoolapp.examplemvvm.data.ExItem;
 import studio.ultoolapp.examplemvvm.databinding.ItemListExampleBinding;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+    // https://ithelp.ithome.com.tw/articles/10193830
+
+    private List<ExItem> itemList;
+
+    public ItemAdapter(List<ExItem> itemList) {
+        this.itemList = itemList;
+    }
 
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemListExampleBinding binding = ItemListExampleBinding.inflate(layoutInflater, parent, false);
+        return new ItemViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
-
+        holder.bindWith(itemList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemList.size();
     }
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
         private ItemListExampleBinding binding;
 
-        public ItemViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ItemViewHolder(@NonNull ItemListExampleBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bindWith(ExItem item) {
+            binding.setItem(item);
+            binding.executePendingBindings();
+            // 即刻更新資料到Layout上。參見： https://stackoverflow.com/questions/53043412/android-why-use-executependingbindings-in-recyclerview
         }
     }
 }
